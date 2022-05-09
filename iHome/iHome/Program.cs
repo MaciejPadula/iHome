c1fdc9164ea34c5c1f160027991a48042fb37d1a
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-05-09 22:58:53 +0200

    ProjectCreated

---
+++ iHome/iHome/Program.cs
@@ -0,0 +1,25 @@
+using iHome;
+
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages(options => {
+    options.RootDirectory = "/wwwroot";
+});
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthorization();
+
+app.MapGet("/hi", () => "Hello!");
+ 
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
