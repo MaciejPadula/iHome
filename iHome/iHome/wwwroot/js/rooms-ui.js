2adc049f62455b5e4f2e7a13e0a595c78fc13587
MaciejPadula <maciejpadula@wp.pl>
2022-06-26 14:58:59 +0200

    Some refactor

--- iHome/iHome/wwwroot/js/rooms-ui.js
+++ iHome/iHome/wwwroot/js/rooms-ui.js
@@ -26,7 +26,7 @@
 
 
     content = "";
-    if (image != "") {
+    if (image != "null" && image!="" && image!=undefined) {
         let img = document.createElement("img");
         img.className = "card-img-top";
         img.src = image;
@@ -48,7 +48,7 @@
     let removeRoomButton = document.createElement("button");
     removeRoomButton.className = "btn remove-button rounded-0";
     removeRoomButton.dataset.roomid = id;
-    removeRoomButton.innerHTML = getRemoveButton();
+    removeRoomButton.innerHTML = getRemoveIcon();
     removeRoomButton.addEventListener("click", removeRoom);
 
     //content += "<input type=\"button\" class=\"\" value=\"Remove\" onClick=\"removeRoom(" + id + ")\"/>";
@@ -236,7 +236,7 @@ function getEditIcon() {
     return svg;
 }
 
-function getRemoveButton() {
+function getRemoveIcon() {
     let svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-trash\" viewBox=\"0 0 16 16\">";
     svg += "<path d=\"M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z\"/>";
     svg += "<path fill-rule=\"evenodd\" d=\"M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z\"/>";
