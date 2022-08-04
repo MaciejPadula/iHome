90f7ac38b2f21d8e7884f2339cbe663a0be23d60
MaciejPadula <maciejpadula@wp.pl>
2022-08-04 19:12:14 +0200

    Logic of application moved to Logic namespace

---
+++ iHome/iHome/Logic/ConfigProvider/IConfigProvider.cs
@@ -0,0 +1,9 @@
+﻿using iHome.Models.Database;
+
+namespace iHome.Logic.ConfigProvider
+{
+    public interface IConfigProvider
+    {
+        DatabaseSettings loadDatabaseSettings(string? filename);
+    }
+}
---
+++ iHome/iHome/Logic/UserInfo/IUserInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace iHome.Logic.UserInfo
+{
+    public interface IUserInfo
+    {
+        Task<string?> GetPublicIp(HttpContext httpContext);
+        string? GetUserUuid(ClaimsPrincipal user);
+        string? GetUserUuid(string? email);
+    }
+}
