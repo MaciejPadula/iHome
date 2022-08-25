6dab6534980cd8f1dc72053104638c330acc5c85
MaciejPadula <maciejpadula@wp.pl>
2022-08-25 22:11:20 +0200

    Frontend routers

---
+++ iHome/ihome.frontend/src/app/pages/index/index.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class IndexComponent implements OnInit {
+
+  constructor(private _auth: AuthService, private _router: Router) {
+    this._auth.isAuthenticated$.subscribe(userAuthenticated => {
+      if(userAuthenticated){
+        this._router.navigate(['/rooms']);
+      }
+    });
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
