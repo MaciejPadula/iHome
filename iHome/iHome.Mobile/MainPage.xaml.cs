ece22423ffd6bf65a7eaff5452a0f29941d1342f
MaciejPadula <maciej.padula@wp.pl>
2022-09-07 22:21:10 +0200

    iHome.Mobile app project added

---
+++ iHome/iHome.Mobile/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using iHome.Core.Models.Application;
+using iHome.Core.Services.DatabaseService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace iHome.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IDatabaseService _databaseService;
+        private readonly ApplicationSettings _applicationSettings;
+        int count = 0;
+
+        public MainPage(IDatabaseService databaseService, IOptions<ApplicationSettings> options)
+        {
+            InitializeComponent();
+
+            _applicationSettings = options.Value;
+            _databaseService = databaseService;
+
+            _databaseService.GetListOfRooms("");
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
