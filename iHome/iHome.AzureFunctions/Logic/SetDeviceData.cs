b8a9dcceb4fbd67570e08410ec478e90c9f15198
MaciejPadula <maciejpadula@wp.pl>
2022-09-13 18:01:28 +0200

    Basic Cache update

--- iHome/iHome.AzureFunctions/Logic/SetDeviceData.cs
+++ iHome/iHome.AzureFunctions/Logic/SetDeviceData.cs
@@ -6,19 +6,21 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using iHome.Core.Logic.Database;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using iHome.Core.Models.Requests;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace iHome.AzureFunctions.Logic
 {
     public class SetDeviceData
     {
+        private readonly MemoryCache _memoryCache;
         private readonly AppDbContext _dbContext;
 
-        public SetDeviceData(AppDbContext dbContext)
+        public SetDeviceData(AppDbContext dbContext, MemoryCache memoryCache)
         {
             _dbContext = dbContext;
+            _memoryCache = memoryCache;
         }
 
         [FunctionName("SetDeviceData")]
@@ -28,6 +30,7 @@ namespace iHome.AzureFunctions.Logic
             var data = JsonConvert.DeserializeObject<DeviceDataRequest>(requestBody);
             var deviceId = data.DeviceId;
             var deviceData = data.DeviceData;
+            _memoryCache.Set<string>(deviceId, null);
             var deviceToUpdate = _dbContext.Devices.Where(device => device.DeviceId == deviceId).FirstOrDefault();
 
             if (deviceToUpdate == null)
