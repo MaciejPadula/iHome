a18f6b9703d655bb2a6a12233b38cc28bd25d4df
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-10-16 17:35:37 +0200

    #44 Program.cs cleanup

--- iHome/iHome.Backend/Program.cs
+++ iHome/iHome.Backend/Program.cs
@@ -1,112 +1,29 @@
-using Auth0.AspNetCore.Authentication;
 using iHome.Backend.Middleware;
-using iHome.Core.Logic.Database;
-using iHome.Core.Logic.UserInfo;
-using iHome.Core.Models.Application;
-using iHome.Core.Services.DevicesService;
-using iHome.Core.Services.RoomsService;
+using iHome.Backend.Services;
 using iHome.Hubs;
-using iHome.Logic.Notificator;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 if (!Directory.Exists("wwwroot"))
     Directory.CreateDirectory("wwwroot");
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddSignalR();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-    options.Audience = builder.Configuration["Auth0:Audience"];
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            var accessToken = context.Request.Query["access_token"];
-
-            // If the request is for our hub...
-            var path = context.HttpContext.Request.Path;
-            if (!string.IsNullOrEmpty(accessToken) &&
-                (path.StartsWithSegments("/roomsHub")))
-            {
-                // Read the token out of the query string
-                context.Token = accessToken;
-            }
-            return Task.CompletedTask;
-        }
-    };
-}).AddAuth0WebAppAuthentication(options =>
-{
-    options.Domain = builder.Configuration["Auth0:Domain"];
-    options.ClientId = builder.Configuration["Auth0:ClientId"];
-    options.Scope = "openid profile email";
-});
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "iHome Rooms API", Version = "v1" });
-    var securitySchema = new OpenApiSecurityScheme
-    {
-        Description = "Using the Authorization header with the Bearer scheme.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-
-    c.AddSecurityDefinition("Bearer", securitySchema);
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { securitySchema, new[] { "Bearer" } }
-    });
-});
-
-var myPolicy = "Angular";
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(myPolicy,
-        policy =>
-        {
-            policy
-                .SetIsOriginAllowed(_ => true)
-                .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials();
-        });
-});
-
-builder.Services.AddControllers();
+const string myPolicy = "Angular";
 
 builder.Services
-    .Configure<ApplicationSettings>(builder.Configuration)
-    .AddDbContext<AppDbContext>(options =>
-    {
-        options.UseSqlServer(builder.Configuration["AzureConnectionString"], sqlOptions =>
-        {
-            sqlOptions.EnableRetryOnFailure();
-        });
-    })
-    .AddScoped<IUserInfo, UserInfo>()
-    .AddScoped<IDevicesService, DevicesService>()
-    .AddScoped<IRoomsService, RoomsService>()
-    .AddScoped<INotificator, Notificator>()
-    .AddScoped<RoomsHub>();
+    .ConfigureSwagger()
+    .ConfigureCors(myPolicy)
+    .ConfigureAuth0Authentication(builder.Configuration);
+
+builder.Services
+    .AddControllers();
+
+builder.Services
+    .AddConfiguration(builder.Configuration)
+    .AddAzureSqlServer(builder.Configuration["AzureConnectionString"])
+    .AddApiServices()
+    .AddSignalRHubs()
+    .AddSignalR();
 
 var app = builder.Build();
 
---
+++ iHome/iHome.Backend/Services/IServiceDatabaseExtensions.cs
@@ -0,0 +1,27 @@
+﻿using iHome.Core.Logic.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace iHome.Backend.Services
+{
+    public static class IServiceDatabaseExtensions
+    {
+        public static IServiceCollection AddAzureSqlServer(this IServiceCollection services, string connectionString)
+        {
+            return services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString, sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure();
+                });
+            });
+        }
+
+        public static IServiceCollection AddAzureCosmosDb(this IServiceCollection services, string connectionString, string databaseName)
+        {
+            return services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseCosmos(connectionString, databaseName);
+            });
+        }
+    }
+}
