f95389230f4f3661ec3b351880d060168e594925
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2022-10-31 00:29:16 +0100

    #49 setup

---
+++ iHome/iHome.WearOS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace iHome.WearOS
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- iHome/iHome.WearOS/Platforms/Tizen/Main.cs
+++ iHome/iHome.WearOS/Platforms/Tizen/Main.cs
@@ -2,7 +2,7 @@ using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
 using System;
 
-namespace iHome.Mobile
+namespace iHome.WearOS
 {
     internal class Program : MauiApplication
     {
