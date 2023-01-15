fa9657e6871d197cd023b9080eee89d195ae4707
Maciej Padula <maciej.padula@wp.pl>
2023-01-15 22:26:24 +0100

    new app added

---
+++ src/iHome.Core/Models/DeviceHub.cs
@@ -0,0 +1,9 @@
+﻿namespace iHome.Core.Models;
+
+public class DeviceHub
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+
+    public required Guid RoomId { get; init; }
+}
---
+++ src/iHome.Core/Models/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace iHome.Core.Models;
+
+public class Room
+{
+    public required Guid Id { get; init; }
+    public required string Name { get; init; }
+
+    public required Guid UserId { get; init; }
+}
