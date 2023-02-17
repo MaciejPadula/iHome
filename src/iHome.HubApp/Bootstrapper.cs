6b1cbb74a5886f35c30f22f3c6c98c5e72631d96
Maciej Padula <maciej.padula@wp.pl>
2023-02-17 23:48:11 +0100

    #61 app added

---
+++ src/iHome.HubApp/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace iHome.HubApp;
+
+public class Bootstrapper
+{
+    public static IServiceCollection Services { get; set; } = new ServiceCollection();
+    private static IServiceProvider? _provier;
+
+    public static T GetService<T>()
+    {
+        _provier ??= Services.BuildServiceProvider();
+        return _provier.GetService<T>() ?? throw new Exception("Service not found");
+    }
+}
---
+++ src/iHome.HubApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iHome.HubApp.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
