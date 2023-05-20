0e045dc874ef16f467e8e4a7d960ea95fcd9d406
MaciejPadula <50674232+MaciejPadula@users.noreply.github.com>
2023-05-20 12:02:34 +0200

    #96 cleanup

--- src/iHome.Infrastructure.OpenAI/Models/OpenAIRequestDevice.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace iHome.Infrastructure.OpenAI.Models;
-
-public class OpenAIRequestDevice
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = default!;
-    public string Type { get; set; } = default!;
-}
--- src/iHome.Infrastructure.OpenAI/Services/Suggestions/ISuggestionsService.cs
+++
@@ -1,9 +0,0 @@
-﻿using iHome.Infrastructure.OpenAI.Models;
-
-namespace iHome.Infrastructure.OpenAI.Services.Suggestions;
-
-public interface ISuggestionsService
-{
-    Task<IEnumerable<Guid>> GetDevicesThatCouldMatchSchedule(string scheduleName, string scheduleTime, IEnumerable<OpenAIRequestDevice> devices);
-    Task<string> GetSuggestedTimeByScheduleName(string scheduleName);
-}
