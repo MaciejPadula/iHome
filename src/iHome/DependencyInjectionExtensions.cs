dd354e581a9802036130de58258fe6c9e81be90d
MaciejPadula <maciej.padula@wp.pl>
2023-05-23 16:44:29 +0200

    #96 zerotier ips

--- src/iHome/DependencyInjectionExtensions.cs
+++ src/iHome/DependencyInjectionExtensions.cs
@@ -12,15 +12,15 @@ public static class DependencyInjectionExtensions
 {
     public static IServiceCollection AddMicroservices(this IServiceCollection services)
     {
-        services.AddMicroserviceClient<IDeviceManagementService>("http://192.168.8.2:5002");
-        services.AddMicroserviceClient<IDeviceDataService>("http://192.168.8.2:5002");
+        services.AddMicroserviceClient<IDeviceManagementService>("http://172.30.0.3:5002");
+        services.AddMicroserviceClient<IDeviceDataService>("http://172.30.0.3:5002");
 
         services.AddMicroserviceClient<IUserManagementService>("http://192.168.8.2:5003");
 
-        services.AddMicroserviceClient<IRoomManagementService>("http://192.168.8.2:5004");
-        services.AddMicroserviceClient<IRoomSharingService>("http://192.168.8.2:5004");
+        services.AddMicroserviceClient<IRoomManagementService>("http://172.30.0.2:5004");
+        services.AddMicroserviceClient<IRoomSharingService>("http://172.30.0.2:5004");
 
-        services.AddMicroserviceClient<ISuggestionsService>("http://192.168.8.2:5005");
+        services.AddMicroserviceClient<ISuggestionsService>("http://172.30.0.4:5005");
 
         services.AddMicroserviceClient<IWidgetManagementService>("http://192.168.8.2:5006");
         services.AddMicroserviceClient<IWidgetDeviceManagementService>("http://192.168.8.2:5006");
