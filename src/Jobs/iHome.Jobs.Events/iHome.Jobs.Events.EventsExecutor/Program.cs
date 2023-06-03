fc3af84ae3ab0f12d77fe30618db2db1aa7ea2be
MaciejPadula <maciej.padula@wp.pl>
2023-06-03 22:53:05 +0200

    #98 some docker fixes

--- src/Jobs/iHome.Jobs.Events/iHome.Jobs.Events.EventsExecutor/Program.cs
+++ src/Jobs/iHome.Jobs.Events/iHome.Jobs.Events.EventsExecutor/Program.cs
@@ -1,8 +1,15 @@
+using iHome.Infrastructure.Queue.Helpers;
 using iHome.Jobs.Events.EventsExecutor;
+using iHome.Microservices.Devices.Contract;
+using Web.Infrastructure.Microservices.Client.Extensions;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
+        var config = context.Configuration;
+
+        services.AddDataQueueWriter(config.GetValue<string>("Azure:StorageConnectionString"));
+        services.AddMicroserviceClient<IDeviceDataService>("http://172.30.0.3:5002");
         services.AddHostedService<Worker>();
     })
     .Build();
